d98344b4e48d168f48237b821f3a1b91abaf59d6
Kuziini-product <madalintomescu@gmail.com>
2025-07-29 22:36:19 +0300

    Update ai_generator.py

--- ai_generator.py
+++ ai_generator.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = OpenAI(
-    api_key=os.getenv(OPENAI_API_KEY)
+    api_key=os.getenv("OPENAI_API_KEY")
 )
 
 def genereaza_deviz_AI(descriere, dimensiuni, baza_date_df):
